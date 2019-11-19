82c095fe233517283cbb79f5327771654abf2c5f
Noah Heinrich <noahheinrich@gmail.com>
2019-11-19 12:06:09 -0600

    Can now access all cards via api

--- card_cloth/app/controllers/api/v1/cards_controller.rb
+++ card_cloth/app/controllers/api/v1/cards_controller.rb
@@ -1,3 +1,10 @@
-class CardsController < ApplicationController
+class Api::V1::CardsController < ApplicationController
+  def index
+    @cards = Card.all
+    render json: @cards, except: [:id, :created_at, :updated_at]
+  end
 
+  def show
+
+  end
 end
